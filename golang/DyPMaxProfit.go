e4a0f999a5245d8918bd1b2000454f1bbacbb80f
Clement Liu <cheerfulliu@hotmail.com>
2021-11-23 20:47:54 +0800

    add DyP solutions

--- golang/DyPMaxProfit.go
+++ golang/DyPMaxProfit.go
@@ -23,8 +23,96 @@ package main
 
 1 <= prices.length <= 105
 0 <= prices[i] <= 104
+
+dp[i]：在第i天能获得的最高利润，
+min ： 最低价
+if price[i] - min  > dp[i] ：更新
+初始化：0
+*/
+
+func maxProfit1(prices []int) int {
+	OldProfit, NewProfit := 0, 0
+
+	min := prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < min {
+			min = prices[i]
+		}
+
+		NewProfit = MaxOf2(OldProfit, prices[i] - min)
+		OldProfit = NewProfit
+	}
+
+
+	return NewProfit
+}
+
+/*
+给定一个数组 prices ，其中prices[i] 是一支给定股票第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+*/
+
+func maxProfit2(prices []int) int {
+	Profit, min := 0, prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < prices[i - 1] {
+			Profit += prices[i - 1] - min
+
+		} else {
+			Profit += prices[i] - prices[i - 1]
+		}
+		min = prices[i]
+	}
+
+	return Profit
+}
+
+
+/*
+给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你最多可以完成两笔交易。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+示例1:
+
+输入：prices = [3,3,5,0,0,3,1,4]
+输出：6
+解释：在第 4 天（股票价格 = 0）的时候买入，在第 6 天（股票价格 = 3）的时候卖出，这笔交易所能获得利润 = 3-0 = 3 。
+    随后，在第 7 天（股票价格 = 1）的时候买入，在第 8 天 （股票价格 = 4）的时候卖出，这笔交易所能获得利润 = 4-1 = 3 。
+示例 2：
+
+输入：prices = [1,2,3,4,5]
+输出：4
+解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+    注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+    因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+示例 3：
+
+输入：prices = [7,6,4,3,1]
+输出：0
+解释：在这个情况下, 没有交易完成, 所以最大利润为 0。
+示例 4：
+
+输入：prices = [1]
+输出：0
+
+
+提示：
+
+1 <=prices.length <= 105
+0 <=prices[i] <=105
 */
 
 func maxProfit(prices []int) int {
+	
+
 
 }
